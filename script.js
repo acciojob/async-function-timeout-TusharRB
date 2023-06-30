38e5dcd1027270a345815d8c74c603ae877bf39b
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-06-30 12:51:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+// script.js
+document.getElementById("btn").addEventListener("click", showMessage);
+
+async function showMessage() {
+  const text = document.getElementById("text").value;
+  const delay = document.getElementById("delay").value;
+
+  try {
+    const result = await waitForDelay(delay, text);
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function waitForDelay(delay, message) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(message);
+    }, delay);
+  });
+}
